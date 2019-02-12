e9095eb5a690746cad5d0cf56a57c35a6798d6e4
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 02:50:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,7 @@ var playlist = {
   Slowdive: 'Alison',
   'My Bloody Valentine': 'Sometimes'
 }
-var artistName = "Phil Ochs"
-var songTitle = "Here\'s to the State of Mississippi"
+
 
 function updatePlaylist(playlist,artistName,songTitle){
   playlist['Phil Ochs'] = 'Here\'s to the State of Mississippi'
